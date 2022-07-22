85a01a47be2f525815034e1f2de7e4e082fa28b1
Gabliz <lizzosgabriele@gmail.com>
2022-07-22 12:33:16 +0200

    added forge starter files

---
+++ forge/src/main/java/it/gabliz/additional/forge/Additional.java
@@ -0,0 +1,38 @@
+package it.gabliz.additional.forge;
+
+import com.mojang.logging.LogUtils;
+import it.gabliz.additional.common.config.ModConfig;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ModConfig.MOD_ID)
+public class Additional {
+    public static Logger LOGGER = LogUtils.getLogger();
+
+    public Additional() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+
+        });
+
+    }
+
+    @Mod.EventBusSubscriber(modid = ModConfig.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
